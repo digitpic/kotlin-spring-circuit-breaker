21339fa33483332537e825e9e22f44178d52707b
digitpic <jkw5033@gmail.com>
2025-02-27 23:00:33 +0900

    chore: initialize

---
+++ src/test/kotlin/com/spring/circuit/breaker/CircuitBreakerApplicationTests.kt
@@ -0,0 +1,11 @@
+package com.spring.circuit.breaker
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CircuitBreakerApplicationTests {
+    @Test
+    fun contextLoads() {
+    }
+}
